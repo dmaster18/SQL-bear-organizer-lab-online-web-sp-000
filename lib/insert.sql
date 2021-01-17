c9a2e32731af3879ea06765ff59a81ad5311f436
David Strauch <dmaster18@gmail.com>
2021-01-17 23:07:45 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "M", "Brown", "Happy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "F", "Orange", "goofy", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 20, "F", "Grey", "Tranquil", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 18, "M", "Brown", "Assertive", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 90, "F", "Yellow", "Ornery", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 35, "M", "Green", "Grumpy", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 17, "F", "White", "Peaceful", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, "M", "Turquoise", "Sad", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr Chocolate", 1, "M", "Brown", "Good", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "M", "Brown", "Mental", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "F", "Black", "Good", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergent Brown", 2, "M", "Brown", "Good", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 1, "F", "Black", "Bitey", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 5, "M", "Grey", "Good", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "F", "Brown", "Good", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 6, "M", "Brown", "Bad", 1); 
